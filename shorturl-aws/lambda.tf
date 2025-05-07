e87f61aef905f24daaca766d76f0c25e783b37a8
josh <joshuaholman5@gmail.com>
2025-05-06 22:08:11 -0700

    update python version

--- shorturl-aws/lambda.tf
+++ shorturl-aws/lambda.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "url_lambda" {
   function_name = "ShortUrlLambda"
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
+  runtime       = "python3.13"
 
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
