b01319bfdaec6935f95cb21fbd59c71f0c31b491
josh <joshuaholman5@gmail.com>
2025-05-06 20:10:07 -0700

    endpoint works

--- shorturl-aws/database.tf
+++ shorturl-aws/database.tf
@@ -2,6 +2,7 @@ resource "aws_dynamodb_table" "url-dynamodb" {
   name         = "Urls"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "shortId"
+  range_key    = "longUrl"
 
   attribute {
     name = "shortId"
