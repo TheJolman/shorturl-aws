2d174dee97c67d7b3c27185dec9001f69b46f368
josh <joshuaholman5@gmail.com>
2025-05-06 22:16:53 -0700

    correct payload format version

--- shorturl-aws/api-gateway.tf
+++ shorturl-aws/api-gateway.tf
@@ -29,6 +29,7 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id             = aws_apigatewayv2_api.url_shortener_api.id
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
+  payload_format_version = "2.0"
   integration_uri    = aws_lambda_function.url_lambda.invoke_arn
 }
 
